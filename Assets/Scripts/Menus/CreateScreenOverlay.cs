ccce9f55f0cc570e412b584c50afc7fb72357d1d
sentobi <howcomee@outlook.com>
2017-02-22 01:45:05 +0800

    - added main menu

--- Assets/Scripts/Menus/CreateScreenOverlay.cs
+++ Assets/Scripts/Menus/CreateScreenOverlay.cs
@@ -7,6 +7,8 @@ public class CreateScreenOverlay : MonoBehaviour {
 
     public void CreateOverlay()
     {
+        OverlayActive.SetOverlayActive(true);
+
         GameObject overlay = (GameObject)Instantiate(overlayToCreate, new Vector3(0, 0, 0), Quaternion.identity);
         overlay.transform.SetParent(this.transform.parent);   // set to this button's canvas
         overlay.transform.localPosition = overlayToCreate.transform.localPosition;
@@ -14,6 +16,9 @@ public class CreateScreenOverlay : MonoBehaviour {
         //popUp.GetChild(0).localScale = new Vector3(0.1f, 0.1f, 1);
 
         overlay.transform.localScale = new Vector3(1, 1, 1);
+
+        RectTransform transform = overlay.GetComponent<RectTransform>();
+        transform.sizeDelta = new Vector2(0f, 0f);
     }
 
 }
