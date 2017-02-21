7a6cd338582a65f337c57510ad3ac759a4719956
pohshania <pohshania@gmail.com>
2017-02-21 17:11:30 +0800

    - radar editeddddd - destroy environment desuuuuu

---
+++ Assets/Scripts/DestroyEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEnvironment : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "TestCube")
+        {
+            //Radar.RemoveRadarObject(col.gameObject);
+            //Destroy(col.gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Radar/Radar.cs
+++ Assets/Scripts/Radar/Radar.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine.UI;
 
-public class RadarObject
+public class RadarIcon
 {
-    public GameObject[] owner;
     public Image currentIcon;
     public Image icon;
     public Image iconHigher;
@@ -23,69 +22,96 @@ public class Radar : MonoBehaviour
     float mapScale = 0.05f;
     int offsetY = 100;
 
+    public static List<GameObject> worldObject = new List<GameObject>();
+    public static List<RadarIcon> radIcons = new List<RadarIcon>();
+
+
     private void SetPlayer(GameObject setPlayer)
     {
         this.player = setPlayer;
     }
-
-    // Array to store all the gameobjects with the tag
-    GameObject[] worldObject; //= GameObject.FindGameObjectsWithTag("WorldObject");
-
-    public static List<RadarObject> radObjects = new List<RadarObject>();
 
     void RegisterRadarObject(Image icon ,Image iconHigher, Image iconLower)
     {
         Image middleIcon = Instantiate(icon);
         Image higherIcon = Instantiate(iconHigher);
         Image lowerIcon = Instantiate(iconLower);
-        // Add into the list
-        radObjects.Add(new RadarObject() { owner = worldObject, icon = middleIcon, iconHigher = higherIcon , iconLower=lowerIcon, currentIcon = middleIcon });
+        // Add into the radar icon list
+        radIcons.Add(new RadarIcon() { icon = middleIcon, iconHigher = higherIcon , iconLower=lowerIcon, currentIcon = middleIcon });
     }
 
-    //public static void RemoveRadarObject(GameObject o)
-    //{
-    //    List<RadarObject> newList = new List<RadarObject>();
-    //    for (int i = 0; i < radObjects.Count; i++)
-    //    {
-    //        if (radObjects[i].owner == o)
-    //        {
-    //            Destroy(radObjects[i].icon);
-    //            continue;
-    //        }
-    //        else
-    //            newList.Add(radObjects[i]);
-    //    }
+    public static void RemoveRadarObject(GameObject go)
+    {
+        //List<RadarObject> newList = new List<RadarObject>();
+        //for (int i = 0; i < radObjects.Count; i++)
+        //{
+        //    if (radObjects[i].owner == o)
+        //    {
+        //        Destroy(radObjects[i].icon);
+        //        continue;
+        //    }
+        //    else
+        //        newList.Add(radObjects[i]);
+        //}
 
-    //    radObjects.RemoveRange(0, radObjects.Count);
-    //    radObjects.AddRange(newList);
-    //}
+        //radObjects.RemoveRange(0, radObjects.Count);
+        //radObjects.AddRange(newList);
+
+
+        // loop through world objects array
+        for (int i = 0; i < worldObject.Count; ++i)
+        {
+            if (worldObject[i] == go)
+            {
+                //worldObject.Remove(worldObject[i]);
+                //radIcons.Remove(radIcons[i]);
+
+
+                //worldObject[i].gameObject.SetActive(false);
+                //radIcons[i].iconHigher.gameObject.SetActive(false);
+                //radIcons[i].iconLower.gameObject.SetActive(false);
+                //radIcons[i].icon.gameObject.SetActive(false);
+
+                //worldObject.RemoveAt(i);
+
+                Debug.Log("hey");
+                continue;
+            }
+            //else
+                //worldObject.AddRange(worldObject);
+        }
+
+        //worldObject.RemoveRange(0, worldObject.Count);
+        //worldObject.AddRange(worldObject);
+    }
 
     void DrawRadarDots()
     {
-        foreach (RadarObject ro in radObjects)
+        for (int i = 0; i < worldObject.Count; ++i)
         {
-            foreach (GameObject go in worldObject)
-            {
-                Vector3 radarPos = (go.transform.position - player.transform.position);
+            Vector3 radarPos = (worldObject[i].transform.position - player.transform.position);
 
-                float distToObject = Vector3.Distance(player.transform.position, go.transform.position) * mapScale;
-                float deltay = Mathf.Atan2(radarPos.x, radarPos.z) * Mathf.Rad2Deg - 270 - player.transform.eulerAngles.y;
-                radarPos.x = distToObject * Mathf.Cos(deltay * Mathf.Deg2Rad) * -1;
-                radarPos.z = distToObject * Mathf.Sin(deltay * Mathf.Deg2Rad);
-                ro.currentIcon.gameObject.transform.SetParent(this.gameObject.transform);
-                //ro.currentIcon.gameObject.transform.SetParent(GameObject.Find("Canvas").transform);
-                ro.currentIcon.gameObject.transform.position = new Vector3(radarPos.x, radarPos.z, 0) + this.transform.position;
-                //ro.transform.SetParent();
-            }
+            float distToObject = Vector3.Distance(player.transform.position, worldObject[i].transform.position) * mapScale;
+            float deltay = Mathf.Atan2(radarPos.x, radarPos.z) * Mathf.Rad2Deg - 270 - player.transform.eulerAngles.y;
+            radarPos.x = distToObject * Mathf.Cos(deltay * Mathf.Deg2Rad) * -1;
+            radarPos.z = distToObject * Mathf.Sin(deltay * Mathf.Deg2Rad);
 
+            radIcons[i].currentIcon.gameObject.transform.SetParent(this.gameObject.transform);
+            radIcons[i].currentIcon.gameObject.transform.position = new Vector3(radarPos.x, radarPos.z, 0) + this.transform.position;
         }
     }
 
     // Use this for initialization
     void Start()
     {
-        worldObject = GameObject.FindGameObjectsWithTag("WorldObject");
-        RegisterRadarObject(icon, iconHigher, iconLower);
+        GameObject[] worldObjArr;
+        worldObjArr = GameObject.FindGameObjectsWithTag("WorldObject");
+        for (int i = 0; i < worldObjArr.Length; ++i)
+        {
+            worldObject.Add(worldObjArr[i]);
+            RegisterRadarObject(icon, iconHigher, iconLower);
+        }
+
 
         // find localplayer & set
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
@@ -103,58 +129,49 @@ public class Radar : MonoBehaviour
     void Update()
     {
         DrawRadarDots();
-        //Debug.Log("hey");
 
-       foreach(RadarObject ro in radObjects)
-       {
-           foreach(GameObject go in worldObject)
-           {
-               // the position of all the gameobjects on the map
-               Vector3 radarPos = go.transform.position;
-               //Debug.Log("Radar Pos"+radarPos);
+        for (int i = 0; i < worldObject.Count; ++i)
+        {
+            // the position of all the gameobjects on the map
+            Vector3 radarPos = worldObject[i].transform.position;
 
-               // the position of players
-               Player playerScript = player.GetComponent<Player>();
-               Vector3 playerPos = PlayerUI.playerPos;//playerScript.transform.position;
-             //  Debug.Log("PlayerPos:" + playerPos);
+            // the position of players
+            Player playerScript = player.GetComponent<Player>();
+            Vector3 playerPos = PlayerUI.playerPos;//playerScript.transform.position;
+      
 
-               // check for gameobject above the player
-               if(radarPos.y > (playerPos.y + offsetY))
-               {
-                  
-                   ro.iconHigher.gameObject.SetActive(true);
-                   ro.iconLower.gameObject.SetActive(false);
-                   ro.icon.gameObject.SetActive(false); 
-                   ro.currentIcon = ro.iconHigher;
-                  // ro.currentIcon.sprite = ro.iconHigher.sprite;
-                   //Debug.Log("Higher"); 
-               }
-               // check for gameobject below the player
-               else if(radarPos.y < (playerPos.y - offsetY))
-               {
-                  
-                   ro.iconHigher.gameObject.SetActive(false);
-                   ro.iconLower.gameObject.SetActive(true);
-                   ro.icon.gameObject.SetActive(false);
-                   ro.currentIcon = ro.iconLower;
-                    // ro.currentIcon.sprite = ro.iconLower.sprite;
-                   //Debug.Log("Lower");
+            // check for gameobject above the player
+            if (radarPos.y > (playerPos.y + offsetY))
+            {
 
-               }
-               // in the middle
-               else
-               {
-                  
-                   ro.iconHigher.gameObject.SetActive(false);
-                   ro.iconLower.gameObject.SetActive(false);
-                   ro.icon.gameObject.SetActive(true); 
-                   ro.currentIcon = ro.icon;
-                    // ro.currentIcon.sprite = ro.icon.sprite;
-                   //Debug.Log("Middle");
-               }
-           }
-       }
+                radIcons[i].iconHigher.gameObject.SetActive(true);
+                radIcons[i].iconLower.gameObject.SetActive(false);
+                radIcons[i].icon.gameObject.SetActive(false);
+                radIcons[i].currentIcon = radIcons[i].iconHigher;
+            }
+            // check for gameobject below the player
+            else if (radarPos.y < (playerPos.y - offsetY))
+            {
+
+                radIcons[i].iconHigher.gameObject.SetActive(false);
+                radIcons[i].iconLower.gameObject.SetActive(true);
+                radIcons[i].icon.gameObject.SetActive(false);
+                radIcons[i].currentIcon = radIcons[i].iconLower;
+            }
+            // in the middle
+            else
+            {
+                radIcons[i].iconHigher.gameObject.SetActive(false);
+                radIcons[i].iconLower.gameObject.SetActive(false);
+                radIcons[i].icon.gameObject.SetActive(true);
+                radIcons[i].currentIcon = radIcons[i].icon;
+            }
+        }
 
     }
-}
 
+
+
+
+
+}
