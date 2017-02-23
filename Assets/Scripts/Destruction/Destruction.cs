3b572d8689b971fbfd64aa462b768ceb889dbe50
pohshania <pohshania@gmail.com>
2017-02-23 21:23:38 +0800

    - testing of destructible objects

---
+++ Assets/Scripts/Destruction/Destruction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destruction : MonoBehaviour {
+
+    public Transform BrokenBall;
+    public Transform effect;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "WorldObject")
+        {
+            BreakDeath();
+        }
+    }
+
+    void BreakDeath()
+    {
+        Debug.Log("BreakDeathRead");
+        Instantiate(BrokenBall, transform.position, BrokenBall.transform.rotation);
+        // instantiate particle effect here
+        // play sound
+        Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
