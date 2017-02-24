a5b4885198dbd57021c6dafe69559b44a233830a
sentobi <howcomee@outlook.com>
2017-02-24 18:06:00 +0800

    - match timer

---
+++ Assets/Scripts/Network/GetMatchTimer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class GetMatchTimer : MonoBehaviour {
+
+    public Text matchTimerText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        matchTimerText.text = NetworkManager.singleton.GetComponent<MatchTimer>().GetSeconds().ToString();
+	}
+}
--- Assets/Scripts/Network/MatchTimer.cs
+++ Assets/Scripts/Network/MatchTimer.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MatchTimer : MonoBehaviour {
 
-    
+    public int minutes;
+
+    private float seconds;  // the set time converted to seconds
 
 	// Use this for initialization
 	void Start () {
-	
+        seconds = (float)(minutes * 60);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (seconds > 0f)
+            seconds -= Time.deltaTime;
 	}
+
+    public float GetSeconds()
+    {
+        return seconds;
+    }
 }
