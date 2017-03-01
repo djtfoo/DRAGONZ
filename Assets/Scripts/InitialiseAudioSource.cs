590fb55bbf5a0f39c49b97c36edba0010c59fe12
sentobi <howcomee@outlook.com>
2017-03-02 07:07:32 +0800

    - made fire sfx sound use the sound volume - added "invisible wall" to scene - android version can quit game by tapping back button twice

---
+++ Assets/Scripts/InitialiseAudioSource.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialiseAudioSource : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        // set volume
+        audioSource.volume = SettingsData.GetSFXVolumeRange();
+
+        // play
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
